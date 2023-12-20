a6e64b26b00c7cd25b00e73e3fb6d76caa78d3be
Kevin Hui <kevinhui98@gmail.com>
2023-12-20 16:16:34 -0500

    remove keys

--- app/src/main/java/com/example/parstagram_k/App.kt
+++ app/src/main/java/com/example/parstagram_k/App.kt
@@ -1,6 +1,7 @@
 package com.example.parstagram_k
 
 import android.app.Application
+import com.parse.BuildConfig
 import com.parse.Parse
 import com.parse.ParseObject
 
@@ -10,9 +11,14 @@ class App : Application() {
         ParseObject.registerSubclass(Post::class.java)
         Parse.initialize(
             Parse.Configuration.Builder(this)
-                .applicationId(getString(R.string.back4app_app_id))
-                .clientKey(getString(R.string.back4app_client_key))
+                .applicationId(APP_ID)
+                .clientKey(CLIENT_KEY)
                 .server(getString(R.string.back4app_server_url))
                 .build());
+    }
+
+    companion object {
+        private const val APP_ID = com.example.parstagram_k.BuildConfig.APP_ID
+        private const val CLIENT_KEY = com.example.parstagram_k.BuildConfig.CLIENT_KEY
     }
 }
